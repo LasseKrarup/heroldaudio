95002a23e45de75cffc45ab9220a2d7f1b4eb6d2
Lasse Krarup <latze95@gmail.com>
2024-03-24 22:27:14 +0100

    Form modifications

--- pages/kontakt.vue
+++ pages/kontakt.vue
@@ -29,6 +29,7 @@
           name="contact"
           data-netlify="true"
           data-netlify-honeypot="bot-field"
+          method="POST"
         >
           <input
             type="hidden"
@@ -86,6 +87,8 @@
                   name="hs-email-contacts-1"
                   id="hs-email-contacts-1"
                   autocomplete="email"
+                  required
+                  aria-required="true"
                   class="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600"
                 />
               </div>
